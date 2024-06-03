8b4070943b15afc34e409e069c5b75ca0a2b101d
Nicodav28 <esteveznicolas0@gmail.com>
2024-06-02 20:55:58 -0500

    🐛 (play.ts): corregir manejo de excepciones en la función playCommand ✨ (play.ts): mejorar la lógica de reproducción de canciones y mensajes de respuesta Se corrigió el manejo de excepciones en la función playCommand para capturar y relanzar correctamente los errores. Además, se mejoró la lógica de reproducción de canciones al verificar si se ha ingresado un valor en el campo de URL y al mostrar mensajes de error adecuados. También se mejoró la presentación de la información de la canción agregada a la cola con un mensaje y una miniatura.

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -69,50 +69,54 @@ const playCommand = {
     client: ExtendedClient,
     interaction: ChatInputCommandInteraction
   ) => {
-    await validateInteraction(interaction);
-    const serverQueue = createServerQueue(client, interaction.guild);
+    try {
+      await validateInteraction(interaction);
+      const serverQueue = createServerQueue(client, interaction.guild);
 
-    if (!serverQueue.connection) {
-      //@ts-expect-error TS not recognizing validations performed before.
-      await serverQueue.connect(interaction.member.voice.channel);
-    }
-
-    const embedElement = new EmbedBuilder();
-
-    if (interaction.options.getSubcommand() === "youtube-song") {
-      const query = interaction.options.getString("song-url");
-
-      if (!query) {
-        await interaction.reply("A value needs to be entered in the field.");
-
-        throw new Error("User leaving url field empty");
+      if (!serverQueue.connection) {
+        //@ts-expect-error TS not recognizing validations performed before.
+        await serverQueue.connect(interaction.member.voice.channel);
       }
 
-      const result = await client.player.search(query, {
-        searchEngine: QueryType.AUTO,
+      const embedElement = new EmbedBuilder();
+
+      if (interaction.options.getSubcommand() === "youtube-song") {
+        const query = interaction.options.getString("song-url");
+
+        if (!query) {
+          await interaction.reply("A value needs to be entered in the field.");
+
+          throw new Error("User leaving url field empty");
+        }
+
+        const result = await client.player.search(query, {
+          searchEngine: QueryType.AUTO,
+        });
+
+        if (result.tracks.length === 0) {
+          return interaction.reply("No tracks found.");
+        }
+
+        const song = result.tracks[0];
+        serverQueue.addTrack(song);
+
+        embedElement
+          .setDescription(
+            `**[${song.title}](${song.url})** has been added to the Queue`
+          )
+          .setThumbnail(song.thumbnail)
+          .setFooter({ text: `Duration: ${song.duration}` });
+      }
+
+      // if (!serverQueue.isPlaying) await serverQueue.play();
+
+      // Respond with the embed containing information about the player
+      await interaction.reply({
+        embeds: [embedElement],
       });
-
-      if (result.tracks.length === 0) {
-        return interaction.reply("No tracks found.");
-      }
-
-      const song = result.tracks[0];
-      serverQueue.addTrack(song);
-
-      embedElement
-        .setDescription(
-          `**[${song.title}](${song.url})** has been added to the Queue`
-        )
-        .setThumbnail(song.thumbnail)
-        .setFooter({ text: `Duration: ${song.duration}` });
+    } catch (error: any) {
+      throw new Error(error.message);
     }
-
-    // if (!serverQueue.isPlaying) await serverQueue.play();
-
-    // Respond with the embed containing information about the player
-    await interaction.reply({
-      embeds: [embedElement],
-    });
   },
 };
 
